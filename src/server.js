5440e9c84079745ee46208c98f084e8c77648db0
le xuan du <dulx96@gmail.com>
2018-12-28 11:08:24 +0700

    done server v1

--- src/server.js
+++ src/server.js
@@ -1,42 +1,51 @@
 var express = require('express')
 var fetch = require('node-fetch')
+var axios = require('axios')
 const FormData = require('form-data')
-var app = express()
+let HOST = 3000
+process.argv.forEach((val, index, array) => {
+    if (val === '-H') HOST = array[index + 1]
+})
+app = express()
 
-var jsObject = {}
+jsObject = {}
 const username = 'fbadoctor@gmail.com'
 const password = 'Avengers@2018'
 
 app.get('/', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     res.send('hello world')
 })
-app.post('/jsLogin', async (req, res) => {
+app.get('/jsLogin', async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
     const result = await loginJS(username, password)
     jsObject = result
+    console.log('login')
     res.json(result)
+})
+app.get('/jsAuth', (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    console.log('auth')
+    res.json(jsObject)
 })
 
 const loginJS = async (username, password) => {
     const formData = new FormData()
-    formData.append('username',username)
+    formData.append('username', username)
     formData.append('password', password)
     formData.append('app', 'jsp')
     try {
         const result = await fetch('https://junglescoutpro.herokuapp.com/api/v1/users/initial_authentication', {
             method: 'POST',
-            mode:'no-cors',
+            mode: 'no-cors',
             body: formData,
             header: {
                 'Content-Type': 'application/json'
             }
         })
-        return json =  await result.json()
+        return json = await result.json()
     } catch (e) {
     }
 
 }
-
-const reAuth = async () => {
-
-}
-app.listen(3000)
+app.listen(HOST)
